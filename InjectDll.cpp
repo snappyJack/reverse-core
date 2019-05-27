e765f257d587b672df80b052efcd8b2eb2d06ecd
snappyJack <zhangzycn@foxmail.com>
2019-05-28 00:01:24 +0800

    Create InjectDll.cpp

---
+++ InjectDll.cpp
@@ -0,0 +1,108 @@
+#include "windows.h"
+#include "tchar.h"
+//用来将myhack.dll注入进程
+BOOL SetPrivilege(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege) 
+{
+    TOKEN_PRIVILEGES tp;
+    HANDLE hToken;
+    LUID luid;
+
+    if( !OpenProcessToken(GetCurrentProcess(),
+                          TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, 
+			              &hToken) )
+    {
+        _tprintf(L"OpenProcessToken error: %u\n", GetLastError());
+        return FALSE;
+    }
+
+    if( !LookupPrivilegeValue(NULL,           // lookup privilege on local system
+                              lpszPrivilege,  // privilege to lookup 
+                              &luid) )        // receives LUID of privilege
+    {
+        _tprintf(L"LookupPrivilegeValue error: %u\n", GetLastError() ); 
+        return FALSE; 
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    if( bEnablePrivilege )
+        tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    else
+        tp.Privileges[0].Attributes = 0;
+
+    // Enable the privilege or disable all privileges.
+    if( !AdjustTokenPrivileges(hToken, 
+                               FALSE, 
+                               &tp, 
+                               sizeof(TOKEN_PRIVILEGES), 
+                               (PTOKEN_PRIVILEGES) NULL, 
+                               (PDWORD) NULL) )
+    { 
+        _tprintf(L"AdjustTokenPrivileges error: %u\n", GetLastError() ); 
+        return FALSE; 
+    } 
+
+    if( GetLastError() == ERROR_NOT_ALL_ASSIGNED )
+    {
+        _tprintf(L"The token does not have the specified privilege. \n");
+        return FALSE;
+    } 
+
+    return TRUE;
+}
+
+BOOL InjectDll(DWORD dwPID, LPCTSTR szDllPath)
+{
+    HANDLE hProcess = NULL, hThread = NULL;
+    HMODULE hMod = NULL;
+    LPVOID pRemoteBuf = NULL;
+    DWORD dwBufSize = (DWORD)(_tcslen(szDllPath) + 1) * sizeof(TCHAR);
+    LPTHREAD_START_ROUTINE pThreadProc;
+
+    // #1. 使用dwPID或取目标进程句柄（然后控制进程）
+    if ( !(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)) )
+    {
+        _tprintf(L"OpenProcess(%d) failed!!! [%d]\n", dwPID, GetLastError());
+        return FALSE;
+    }
+
+    // #2. 在目标进程内存中分配szdllname大小的内存
+    pRemoteBuf = VirtualAllocEx(hProcess, NULL, dwBufSize, MEM_COMMIT, PAGE_READWRITE);//返回值为分配所得缓冲区的地址（notepad内存地址）
+
+    // #3. 将dll写入分配的内存中
+    WriteProcessMemory(hProcess, pRemoteBuf, (LPVOID)szDllPath, dwBufSize, NULL);
+
+    // #4. 或取LoadLibraryW() API的地址     ps:妙
+    hMod = GetModuleHandle(L"kernel32.dll");
+    pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hMod, "LoadLibraryW");
+	
+    // #5. 在进程中运行LoadLibraryW线程
+    hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, pRemoteBuf, 0, NULL);
+    WaitForSingleObject(hThread, INFINITE);	
+
+    CloseHandle(hThread);
+    CloseHandle(hProcess);
+
+    return TRUE;
+}
+
+int _tmain(int argc, TCHAR *argv[])//检查输入的参数，然后调用InjectDll函数
+{
+    if( argc != 3)
+    {
+        _tprintf(L"USAGE : %s <pid> <dll_path>\n", argv[0]);
+        return 1;
+    }
+
+    // change privilege
+    if( !SetPrivilege(SE_DEBUG_NAME, TRUE) )
+        return 1;
+
+    // inject dll
+    if( InjectDll((DWORD)_tstol(argv[1]), argv[2]) )
+        _tprintf(L"InjectDll(\"%s\") success!!!\n", argv[2]);
+    else
+        _tprintf(L"InjectDll(\"%s\") failed!!!\n", argv[2]);
+
+    return 0;
+}
